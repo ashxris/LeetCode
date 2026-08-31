e63b5bf5a93a3682b043af08f8e876d7e836b45d
Ashwin ;) <190212878+ashxris@users.noreply.github.com>
2026-09-01 00:40:14 +0530

    LC-min-stack [Runtime: N/A; Memory: N/A]

---
+++ stack/min-stack.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.s = []
+
+    def push(self, value: int) -> None:
+        self.s.insert(0,value)
+
+    def pop(self) -> None:
+        if len(self.s)==0:
+            raise Exception("Stack is empty.")
+        else:
+            self.s.pop(0)
+
+    def top(self) -> int:
+        if len(self.s)==0:
+            raise Exception("Stack is empty.")
+        else :
+            return self.s[0]
+
+    def getMin(self) -> int:
+        return min(self.s)
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
