68690d4dbf7410f2431b31997222a572558c1367
Ashwin ;) <190212878+ashxris@users.noreply.github.com>
2026-09-01 00:21:51 +0530

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -9,7 +9,7 @@ class Solution:
             }
 
         for item in s:
-            if len(stk)>0 and item in [')', '}', ']'] and dict.get(item, None) == stk[0]:
+            if len(stk)>0 and item in ')]}' and dict.get(item, None) == stk[0]:
                 stk.pop(0)
                 continue
             else:
