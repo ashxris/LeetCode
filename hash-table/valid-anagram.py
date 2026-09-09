af8923903b1f51d487c925893d77920f016f469c
Ashwin ;) <190212878+ashxris@users.noreply.github.com>
2026-09-09 19:58:58 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -5,13 +5,13 @@ class Solution:
         dict2 = {}
 
         for i in s:
-            if i in dict1.keys():
+            if i in dict1:
                 dict1[i]+=1
             else:
                 dict1[i]=1
         
         for i in t:
-            if i in dict2.keys():
+            if i in dict2:
                 dict2[i]+=1
             else:
                 dict2[i]=1
