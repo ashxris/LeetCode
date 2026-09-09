23a22ae727d1a127c129cc2072d1a4ad59d00c47
Ashwin ;) <190212878+ashxris@users.noreply.github.com>
2026-09-09 19:54:40 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

---
+++ sorting/valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        dict1 = {}
+        dict2 = {}
+
+        for i in s:
+            if i in dict1.keys():
+                dict1[i]+=1
+            dict1[i]=1
+        
+        for i in t:
+            if i in dict2.keys():
+                dict2[i]+=1
+            dict2[i]=1
+
+        if dict1 == dict2:
+            return True
+        return False
