7241ca84dc07dd457ab140505c6680c96109acbc
Ashwin ;) <190212878+ashxris@users.noreply.github.com>
2026-09-09 20:06:26 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -8,15 +8,9 @@ class Solution:
             return False
 
         for i in s:
-            if i in dict1:
-                dict1[i]+=1
-            else:
-                dict1[i]=1
+            dict1[i] = dict1.get(i, 0)+1
         
         for i in t:
-            if i in dict2:
-                dict2[i]+=1
-            else:
-                dict2[i]=1
+            dict2[i] = dict2.get(i, 0)+1
 
         return dict1 == dict2
