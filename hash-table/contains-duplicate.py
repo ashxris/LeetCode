be36d0194e1831aef2fd264bcb8cc68f0dd34628
Ashwin ;) <190212878+ashxris@users.noreply.github.com>
2026-09-09 19:31:16 +0530

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

--- hash-table/contains-duplicate.py
+++ hash-table/contains-duplicate.py
@@ -2,6 +2,7 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         temp = set()
         for i in nums:
-            if i in temp: return True
+            if i in temp: 
+                return True
             temp.add(i)
         return False
